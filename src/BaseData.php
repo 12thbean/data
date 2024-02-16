1fc3f2518917263c10a2abfca33db5b03ff47e49
Igor Filippov <thisfil@ya.ru>
2024-02-16 04:59:04 +0300

    refactored BaseData.php

--- src/BaseData.php
+++ src/BaseData.php
@@ -2,9 +2,7 @@
 
 namespace Zendrop\Data;
 
-use Attribute;
 use InvalidArgumentException;
-use ReflectionAttribute;
 use ReflectionClass;
 use ReflectionNamedType;
 use ReflectionParameter;
@@ -19,148 +17,146 @@ abstract class BaseData
     public static function from(array $payload): static
     {
         $reflectionClass = new ReflectionClass(static::class);
-
         $constructor = $reflectionClass->getConstructor();
         $parameters = $constructor->getParameters();
 
-        $resolvedParameters = [];
-
-        $classNameMapper = null;
-        if (self::hasAttribute($reflectionClass, MapInputName::class)) {
-            $classNameMapper = self::getAttributeValue($reflectionClass, MapInputName::class);
-        }
-
-        foreach ($parameters as $reflectionParameter) {
-            $propertyNameMapper = null;
-            if (self::hasAttribute($reflectionParameter, MapInputName::class)) {
-                $propertyNameMapper = self::getAttributeValue($reflectionParameter, MapInputName::class);
-            }
-
-            $resolvedParameters[] = self::resolveParameterValue($reflectionParameter, $payload, $classNameMapper ?? $propertyNameMapper);
-        }
+        $resolvedParameters = array_map(
+            fn($parameter) => self::resolveParameter($parameter, $payload, $reflectionClass),
+            $parameters
+        );
 
         return $reflectionClass->newInstanceArgs($resolvedParameters);
     }
 
-    private static function resolveParameterValue(
-        ReflectionParameter $parameter,
-        array $payload,
-        ?string $nameMapperClass
-    ): int|float|string|bool|array|null|BaseData {
-        $name = ($nameMapperClass !== null)
-            ? (new ($nameMapperClass))->map($parameter->getName())
-            : $parameter->getName();
+    private static function resolveParameter(ReflectionParameter $parameter, array $payload, ReflectionClass $reflectionClass): mixed
+    {
+        $nameMapperClass = self::getNameMapper($parameter, $reflectionClass);
+        $name = self::getParameterName($parameter, $nameMapperClass);
+        $value = self::getParameterValue($payload, $name, $parameter);
 
-        $value = $payload[$name] ?? null;
+        return self::validateAndConvertValue($value, $parameter);
+    }
 
-        if ($value === null && !self::isParameterNullable($parameter)) {
-            throw new CannotCreateData(
-                "Missing required parameter `{$parameter->getName()}`."
-            );
+    /**
+     * @return class-string<NameMapperInterface>|null
+     */
+    private static function getNameMapper(ReflectionParameter $parameter, ReflectionClass $reflectionClass): ?string
+    {
+        foreach ([$reflectionClass, $parameter] as $attributable) {
+            if (self::hasAttribute($attributable, MapInputName::class)) {
+                return self::getAttributeValue($attributable, MapInputName::class);
+            }
         }
 
+        return null;
+    }
+
+    private static function getParameterName(ReflectionParameter $parameter, ?string $nameMapperClass): string
+    {
+        return $nameMapperClass ? (new $nameMapperClass())->map($parameter->getName()) : $parameter->getName();
+    }
+
+    private static function getParameterValue(array $payload, string $name, ReflectionParameter $parameter): mixed
+    {
+        if (!array_key_exists($name, $payload) && !self::isParameterNullable($parameter)) {
+            throw new CannotCreateData("Missing required parameter `{$name}`.");
+        }
+
+        return $payload[$name] ?? null;
+    }
+
+    private static function validateAndConvertValue(mixed $value, ReflectionParameter $parameter): mixed
+    {
         if (is_array($value)) {
-            return self::handleValueWithTypeArray($parameter, $value);
+            return self::handleArrayValue($value, $parameter);
         }
 
-        if (!self::isValueTypeAcceptableByParameter($value, $parameter)) {
-            throw new CannotCreateData(
-                "The provided value for parameter `{$parameter->getName()}` has not expected type " . gettype(
-                    $value
-                )
-            );
-        }
-
+        self::ensureValueType($value, $parameter);
         return $value;
+    }
+
+    private static function handleArrayValue(array $value, ReflectionParameter $parameter): mixed
+    {
+        if (self::isBaseDataSubclassExpectedByParameter($parameter)) {
+            return self::instantiateBaseDataSubclassFromArray($value, $parameter);
+        }
+
+        return self::hasAttribute($parameter, ArrayOf::class)
+            ? self::convertArrayUsingArrayOfAttribute($value, $parameter)
+            : $value;
+    }
+
+    private static function instantiateBaseDataSubclassFromArray(array $value, ReflectionParameter $parameter): BaseData
+    {
+        $subclassName = self::getExpectedBaseDataSubclass($parameter);
+        return $subclassName::from($value);
+    }
+
+    private static function convertArrayUsingArrayOfAttribute(array $array, ReflectionParameter $parameter): array
+    {
+        $expectedItemsType = self::getAttributeValue($parameter, ArrayOf::class);
+        return array_map(
+            fn($item) => self::convertItemAccordingToType($item, $expectedItemsType, $parameter),
+            $array
+        );
+    }
+
+    private static function convertItemAccordingToType($item, string $expectedItemsType, ReflectionParameter $parameter): mixed
+    {
+        if (is_subclass_of($expectedItemsType, BaseData::class)) {
+            if (!is_array($item)) {
+                throw new RuntimeException("Expected array for nested BaseData object creation in `{$parameter->getName()}`.");
+            }
+            return $expectedItemsType::from($item);
+        }
+
+        self::ensureBuiltinType($item, $expectedItemsType, $parameter);
+        return $item;
+    }
+
+    private static function ensureValueType(mixed $value, ReflectionParameter $parameter): void
+    {
+        if (!self::isValueTypeAcceptableByParameter($value, $parameter)) {
+            $typeName = gettype($value);
+            throw new CannotCreateData("Invalid type `{$typeName}` for parameter `{$parameter->getName()}`.");
+        }
+    }
+
+    private static function getAttributeValue(ReflectionParameter|ReflectionClass $attributable, string $attributeClassName): string
+    {
+        $attributes = $attributable->getAttributes($attributeClassName);
+        if (count($attributes) > 1) {
+            throw new RuntimeException("Multiple `$attributeClassName` attributes found in `{$attributable->getName()}`.");
+        } elseif (count($attributes) === 0) {
+            throw new RuntimeException("`{$attributeClassName}` attribute not found in `{$attributable->getName()}`.");
+        }
+
+        $attribute = array_pop($attributes);
+        return $attribute->getArguments()[0];
+    }
+
+    private static function hasAttribute(ReflectionParameter|ReflectionClass $attributable, string $attributeClassName): bool
+    {
+        return count($attributable->getAttributes($attributeClassName)) > 0;
     }
 
     private static function isParameterNullable(ReflectionParameter $parameter): bool
     {
-        $expectedTypes = self::getParameterExpectedTypes($parameter);
-
-        if ($expectedTypes === null) {
-            return true;
-        }
-
-        foreach ($expectedTypes as $expectedType) {
-            if ($expectedType->allowsNull()) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private static function handleValueWithTypeArray(ReflectionParameter $parameter, array $array): array|BaseData
-    {
-        if (self::isBaseDataSubclassExpectedByParameter($parameter)) {
-            /** @var class-string<BaseData> $subclassName */
-            $subclassName = self::getExpectedBaseDataSubclass($parameter);
-            return $subclassName::from($array);
-        }
-
-        if (!self::hasAttribute($parameter, ArrayOf::class)) {
-            return $array;
-        }
-
-        return self::handleValueWithTypeArrayAndArrayOfAttribute($parameter, $array);
-    }
-
-    private static function handleValueWithTypeArrayAndArrayOfAttribute(
-        ReflectionParameter $parameter,
-        array $array
-    ): array {
-        /** @var class-string<BaseData>|string $expectedItemsType */
-        $expectedItemsType = self::getAttributeValue($parameter, ArrayOf::class);
-
-        $isSubclassExpected = false;
-        if (class_exists($expectedItemsType)) {
-            if (!is_subclass_of($expectedItemsType, BaseData::class)) {
-                throw new \Exception(
-                    "ArrayOf attribute of `{$parameter->getName()}` does not contain child of " . BaseData::class
-                );
-            }
-
-            $isSubclassExpected = true;
-        }
-
-
-        $result = [];
-
-        foreach ($array as $key => $item) {
-            if ($isSubclassExpected) {
-                if (!is_array($item)) {
-                    throw new \Exception(
-                        "`{$parameter->getName()}` parameter should contains array of arrays, but " . gettype(
-                            $item
-                        ) . " given in `{$key}` key of payload"
-                    );
-                }
-
-                $result[] = $expectedItemsType::from($item);
-            } else {
-                if (!self::validateBuiltinType($expectedItemsType, $item)) {
-                    throw new \Exception(
-                        "All the items of `{$parameter->getName()}` should be type of `{$expectedItemsType}`"
-                    );
-                }
-
-                $result[] = $item;
-            }
-        }
-
-        return $result;
+       return self::isValueTypeAcceptableByParameter(null, $parameter);
     }
 
     private static function isValueTypeAcceptableByParameter(mixed $value, ReflectionParameter $parameter): bool
     {
         $expectedTypes = self::getParameterExpectedTypes($parameter);
-
         if ($expectedTypes === null) {
-            return true;
+            return true; // No type implies any type is acceptable
         }
 
         foreach ($expectedTypes as $expectedType) {
+            if ($value === null && $expectedType->allowsNull()) {
+                return true;
+            }
+
             if (self::validateBuiltinType($expectedType->getName(), $value)) {
                 return true;
             }
@@ -169,56 +165,44 @@ abstract class BaseData
         return false;
     }
 
-
     /**
-     * @throws \Exception
+     * @return array<int, ReflectionNamedType>|null
      */
-    private static function getExpectedBaseDataSubclass(ReflectionParameter $parameter): string
+    private static function getParameterExpectedTypes(ReflectionParameter $parameter): ?array
     {
-        $expectedTypes = self::getParameterExpectedTypes($parameter);
-        if ($expectedTypes === null) {
-            throw new \Exception("Parameter type is missed.");
+        $type = $parameter->getType();
+        if (!$type) {
+            return null;
         }
 
-        $subclasses = [];
-        foreach ($expectedTypes as $expectedType) {
-            if (class_exists($expectedType) && is_subclass_of($expectedType->getName(), BaseData::class)) {
-                $subclasses[] = $expectedType->getName();
-            }
-        }
+        return ($type instanceof \ReflectionUnionType)
+            ? $type->getTypes()
+            : [$type];
+    }
 
-        if (count($subclasses) > 1) {
-            throw new \Exception(
-                "Error in `{$parameter->getName()}` parameter. More than one data types is not supported."
-            );
-        }
-
-        if (count($subclasses) === 0) {
-            throw new \Exception("No any data type is found for parameter `{$parameter->getName()}`.");
-        }
-
-        return $subclasses[0];
+    private static function validateBuiltinType(string $typeName, mixed $value): bool
+    {
+        return match ($typeName) {
+            'int' => is_int($value),
+            'string' => is_string($value),
+            'float' => is_float($value),
+            'bool' => is_bool($value),
+            'array' => is_array($value),
+            'mixed' => true,
+            'null' => is_null($value),
+            default => throw new InvalidArgumentException("Unsupported type `{$typeName}` for validation."),
+        };
     }
 
     private static function isBaseDataSubclassExpectedByParameter(ReflectionParameter $parameter): bool
     {
-        return self::isTypeAmongParameterExpectedTypes(BaseData::class, $parameter);
-    }
-
-    private static function isTypeAmongParameterExpectedTypes(string $typeOrClass, ReflectionParameter $parameter): bool
-    {
         $expectedTypes = self::getParameterExpectedTypes($parameter);
-
-        if ($expectedTypes === null) {
+        if (!$expectedTypes) {
             return false;
         }
 
         foreach ($expectedTypes as $expectedType) {
-            if (class_exists($expectedType->getName()) && is_a($expectedType->getName(), $typeOrClass, true)) {
-                return true;
-            }
-
-            if ($expectedType->getName() === $typeOrClass) {
+            if (is_a($expectedType->getName(), BaseData::class, true)) {
                 return true;
             }
         }
@@ -226,61 +210,26 @@ abstract class BaseData
         return false;
     }
 
-    private static function getAttributeValue(ReflectionParameter|ReflectionClass $attributable, string $attributeClassName): string
+    private static function getExpectedBaseDataSubclass(ReflectionParameter $parameter): string
     {
-        $attributes = $attributable->getAttributes($attributeClassName);
-
-        if (count($attributes) > 1) {
-            throw new \Exception("$attributable->name contains more than one `{$attributeClassName}` attribute");
-        } elseif (count($attributes) === 0) {
-            throw new \Exception("$attributable->name does not contains `{$attributeClassName}` attribute");
+        $expectedTypes = self::getParameterExpectedTypes($parameter);
+        if (!$expectedTypes) {
+            throw new RuntimeException("Expected type for parameter `{$parameter->getName()}` is missing.");
         }
 
-        /** @var ReflectionAttribute|null $attribute */
-        $attribute = array_pop($attributes);
-
-        return $attribute->getArguments()[0];
-    }
-
-    /**
-     * @param  class-string<Attribute>  $attributeClassName
-     */
-    private static function hasAttribute(ReflectionParameter|ReflectionClass $attributable, string $attributeClassName): bool
-    {
-        return count($attributable->getAttributes($attributeClassName)) > 0;
-    }
-
-    /**
-     * @param  ReflectionParameter  $parameter
-     *
-     * @return ReflectionNamedType[]|null
-     */
-    private static function getParameterExpectedTypes(ReflectionParameter $parameter): ?array
-    {
-        $type = $parameter->getType();
-
-        if ($type === null) {
-            return null;
+        foreach ($expectedTypes as $expectedType) {
+            if (is_subclass_of($expectedType->getName(), BaseData::class)) {
+                return $expectedType->getName();
+            }
         }
 
-        return $type instanceof \ReflectionUnionType
-            ? $type->getTypes()
-            : [$type];
+        throw new RuntimeException("Expected BaseData subclass for parameter `{$parameter->getName()}` is not found.");
     }
 
-    private static function validateBuiltinType(string $typeName, string|int|float|null|bool|array $value): bool
+    private static function ensureBuiltinType(mixed $item, string $expectedType, ReflectionParameter $parameter): void
     {
-        return match ($typeName) {
-            'null' => is_null($value),
-            'int' => is_int($value),
-            'string' => is_string($value),
-            'float' => is_float($value) || is_int($value),
-            'bool' => is_bool($value),
-            'array' => is_array($value),
-            'mixed' => true,
-            default => throw new InvalidArgumentException(
-                "Unsupported type `{$typeName}` for builtin type validation."
-            ),
-        };
+        if (!self::validateBuiltinType($expectedType, $item)) {
+            throw new RuntimeException("Item of parameter `{$parameter->getName()}` does not match expected type `{$expectedType}`.");
+        }
     }
 }
